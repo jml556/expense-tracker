fa95310870156ade9ecd53d1002cafb99caa65b9
jml556 <59402202+jml556@users.noreply.github.com>
2022-07-30 20:49:15 -0400

    added tailwind css to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-200 h-screen flex items-center">
+      <div className="mx-auto max-w-sm bg-white rounded p-3 shadow-lg">
+        <h1>Hello World</h1>
+        <div className="flex">
+          <div>First Container</div>
+          <div>Second Container</div>
+        </div>
+      </div>
     </div>
   );
 }
